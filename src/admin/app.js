418ac06ca2c43dd1f05509304809849c7d552931
unknown <vishavjeet@scizers.com>
2024-08-26 16:41:56 +0530

    web hook

---
+++ src/admin/app.js
@@ -0,0 +1,15 @@
+import ComponentMain from "../extensions/components/mainBtn";
+import ComponentStage from "../extensions/components/stageBtn";
+
+export default {
+  bootstrap(app) {
+    app.injectContentManagerComponent("listView", "actions", {
+      name: "MainBuild",
+      Component: ComponentMain,
+    });
+    app.injectContentManagerComponent("listView", "actions", {
+        name: "StageBuild",
+        Component: ComponentStage,
+      });
+  },
+};
---
+++ src/extensions/components/stageBtn/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import { Button } from "@strapi/design-system/Button";
+// import { Filter } from "@strapi/icons";
+
+const ComponentStage = () => {
+  async function handleChange() {
+    const url = "https://webhook-test.com/2136892cc68c1ea31501b5b238935fb7"
+    try {
+      const response = await fetch(url, { method: "POST" });
+      if (response.ok) {
+        alert("Success")
+      }
+      else{
+        alert("Fail")
+      }
+    } catch (error) {
+      alert("Error")
+    }
+  }
+    return (
+        <button style={{border:"1px solid lightGrey", borderRadius:"5px", padding:"10px", backgroundColor:"white", fontSize:14}} onClick={handleChange} >
+            Stage Build
+        </button>
+    );
+};
+
+export default ComponentStage;
