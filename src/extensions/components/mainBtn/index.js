c16be73fbb1455f33148b32bdf7ee1451d17854c
vishavjeet1999 <vishavjeetsharma88@gmail.com>
2024-11-08 13:34:51 +0530

    webhooks updated

--- src/extensions/components/mainBtn/index.js
+++ src/extensions/components/mainBtn/index.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const ComponentMain = () => {
   async function handleChange() {
-    const url = "https://api.vercel.com/v1/integrations/deploy/prj_EJKj9f3ITuknNf46aDrwqvzPiTks/WnGCNERaYJ"
-    try {
-      const response = await fetch(url, { method: "POST" });
-      if (response.ok) {
-        alert("Success")
+    const confirmed = window.confirm("Are you sure to make main build?");
+    if (confirmed) {
+      const url = "https://api.vercel.com/v1/integrations/deploy/prj_bBK2iWNxDhKDYaslBmKfiWuosYiy/4iziqSOBBr"
+      try {
+        const response = await fetch(url, { method: "POST" });
+        if (response.ok) {
+          alert("Main build Scheduled, changes will be live shortly")
+        }
+        else {
+          alert("Fail")
+        }
+      } catch (error) {
+        alert("Error")
       }
-      else{
-        alert("Fail")
-      }
-    } catch (error) {
-      alert("Error")
     }
+
   }
   return (
     <button style={{ border: "1px solid lightGrey", borderRadius: "4px", padding: "10px", backgroundColor: "white", fontSize: 14 }} onClick={handleChange} >
--- src/extensions/components/stageBtn/index.js
+++ src/extensions/components/stageBtn/index.js
@@ -2,24 +2,28 @@ import React from "react";
 
 const ComponentStage = () => {
   async function handleChange() {
-    const url = "https://api.vercel.com/v1/integrations/deploy/prj_EJKj9f3ITuknNf46aDrwqvzPiTks/WnGCNERaYJ"
-    try {
-      const response = await fetch(url, { method: "POST" });
-      if (response.ok) {
-        alert("Success")
+    const confirmed = window.confirm("Are you sure to make stage build?");
+    if (confirmed) {
+      const url = "https://api.vercel.com/v1/integrations/deploy/prj_4HoJmWRcSZMJyMjO2dAPPx8JDuFI/PUnJMQGaZE"
+      try {
+        const response = await fetch(url, { method: "POST" });
+        if (response.ok) {
+          alert("Stage build Scheduled, changes will be live shortly")
+        }
+        else {
+          alert("Fail")
+        }
+      } catch (error) {
+        alert("Error")
       }
-      else{
-        alert("Fail")
-      }
-    } catch (error) {
-      alert("Error")
     }
+
   }
-    return (
-        <button style={{border:"1px solid lightGrey", borderRadius:"4px", padding:"10px", backgroundColor:"white", fontSize:14}} onClick={handleChange} >
-            Stage Build
-        </button>
-    );
+  return (
+    <button style={{ border: "1px solid lightGrey", borderRadius: "4px", padding: "10px", backgroundColor: "white", fontSize: 14 }} onClick={handleChange} >
+      Stage Build
+    </button>
+  );
 };
 
 export default ComponentStage;
